aeb287c8ee8ec8c3635eab0b28e4a987b9a21762
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-09 20:21:13 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -28,3 +28,22 @@ class StudentsController < ApplicationController
     params.require(:student).permit(:name, :birthday, :hometown)
   end
 end
+
+#         Prefix Verb   URI Pattern                    Controller#Action
+#     classrooms GET    /classrooms(.:format)          classrooms#index
+#                POST   /classrooms(.:format)          classrooms#create
+#  new_classroom GET    /classrooms/new(.:format)      classrooms#new
+# edit_classroom GET    /classrooms/:id/edit(.:format) classrooms#edit
+#      classroom GET    /classrooms/:id(.:format)      classrooms#show
+#                PATCH  /classrooms/:id(.:format)      classrooms#update
+#                PUT    /classrooms/:id(.:format)      classrooms#update
+#                DELETE /classrooms/:id(.:format)      classrooms#destroy
+#       students GET    /students(.:format)            students#index
+#                POST   /students(.:format)            students#create
+#    new_student GET    /students/new(.:format)        students#new
+#   edit_student GET    /students/:id/edit(.:format)   students#edit
+#        student GET    /students/:id(.:format)        students#show
+#                PATCH  /students/:id(.:format)        students#update
+#                PUT    /students/:id(.:format)        students#update
+#                DELETE /students/:id(.:format)        students#destroy
+
