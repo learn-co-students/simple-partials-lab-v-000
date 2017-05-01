a335d38d35b91c781c8407fc1ca619db2684c5c1
Logan McAnsh <logan@mcan.sh>
2017-05-01 14:45:54 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,22 +13,22 @@
 ActiveRecord::Schema.define(version: 20160119215120) do
 
   create_table "classroom_students", force: :cascade do |t|
-    t.integer  "classroom_id"
-    t.integer  "student_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer "classroom_id"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "classrooms", force: :cascade do |t|
-    t.string   "course_name"
-    t.string   "semester"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "course_name"
+    t.string "semester"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.string   "hometown"
+    t.string "name"
+    t.string "hometown"
     t.datetime "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
