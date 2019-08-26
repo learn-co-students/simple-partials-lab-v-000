bbd8e455893be5beb00ea5629401df9331d6f811
github username <github email address>
2019-08-26 11:00:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,18 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+=begin
+
+#edit and new html erb files
+
+<%= form_for @student do |f|%>
+ <%= render 'form' %>
+<% end %>
+
+Also, spec file needed to be changed due to update in .between method
+  
+=end
+
   def create
     @student = Student.new(student_params)
     if @student.save
